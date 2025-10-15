466e76d6c1ab7347b1039546d6a86ecd1845c327
AaronChengHao <616915054@qq.com>
2025-10-15 14:00:05 +0800

    add erc20 openzeppelin

--- FundMe_flattened.sol
+++ FundMe_flattened.sol
@@ -1,7 +1,31 @@
+
+// File: @chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol
+
+
+pragma solidity ^0.8.0;
+
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
+// File: FundMe.sol
+
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 // 1. 创建一个收款函数
 // 2. 记录投资人并且查看
